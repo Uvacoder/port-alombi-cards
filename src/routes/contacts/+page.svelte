1d382b322dc7120baa5c76f2690c89166d5e7ea6
uvacoder <uva22940coder@gmail.com>
2024-03-02 03:36:41 -0500

    Update +page.svelte

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -4,4 +4,4 @@
 
 <Header lang="en" />
 <h1>Contacts</h1>
-<p>Feel free to contact me on any of these platforms: <a target="_blank" rel="noreferrer" href="https://github.com/alombi">github</a> <a target="_blank" rel="noreferrer" href="https://twitter.com/al0mbi">twitter</a> <a target="_blank" rel="noreferrer" href="https://reddit.com/u/alelombi">reddit</a></p>
+<p>Feel free to contact me on any of these platforms: <a target="_blank" rel="noreferrer" href="https://github.com/uvacoder">github</a> <a target="_blank" rel="noreferrer" href="https://twitter.com/VaLandscapes">twitter</a> <a target="_blank" rel="noreferrer" href="https://codepen.com/uvacoder">codepen</a></p>
